7d0399c3470fa23501dc2b5d536b0c37269f68fd
zhangwei <422430067@qq.com>
2018-12-09 16:08:13 +0800

    添加音频图

--- src/views/recorderjs/recorderjs.vue
+++ src/views/recorderjs/recorderjs.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="recorder-js-test">
+  <div class="recorder-js-demo">
     <h4>录音时间： {{duration}}秒</h4>
     <el-button size="small" @click="initMedia()">初始化</el-button>
-    <el-button size="small" @click="destroyMedia()">结束</el-button>
+    <!-- <el-button size="small" @click="destroyMedia()">结束</el-button> -->
     <el-button size="small" @click="startRecording()">开始录音</el-button>
     <el-button size="small" @click="stopRecording()">结束录音</el-button>
     <el-button size="small" @click="downloadMedia()">下载录音</el-button>
 
-    <wave-stream :stream="streamData"></wave-stream>
+    <!-- <wave-stream :stream="streamData"></wave-stream> -->
+    <!-- 音频图 -->
+    <div class="wave-stream-demo">
+      <svg ref="visualizer" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+          <mask ref="mask" id="mask"><g id="maskGroup"></g></mask>
+          <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" style="stop-color:#ff0a0a;stop-opacity:1" />
+            <stop offset="20%" style="stop-color:#f1ff0a;stop-opacity:1" />
+            <stop offset="90%" style="stop-color:#d923b9;stop-opacity:1" />
+            <stop offset="100%" style="stop-color:#050d61;stop-opacity:1" />
+          </linearGradient>
+        </defs>
+        <rect x="0" y="0" width="100%" height="100%" fill="url(#gradient)" mask="url(#mask)"></rect>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -15,11 +30,8 @@
 import { Message } from 'element-ui'
 import Recorder from 'recorder-js'
 
-import WaveStream from './wave-stream'
-
 export default {
   name: 'recorder-js-test',
-  components: { WaveStream },
   data() {
     return {
       streamData: null,
@@ -36,34 +48,33 @@ export default {
   methods: {
     // 初始化
     initMedia() {
+      const that = this
       const audioContext = new (window.AudioContext || window.webkitAudioConetxt)()
       this.recorder = new Recorder(audioContext, {
         // An array of 255 Numbers
         // You can use this to visualize the audio stream
-        // If you use react, check out react-wave-stream
         // onAnalysed: data => console.log(data)
       })
-
-      const that = this
       navigator.mediaDevices
         .getUserMedia({ audio: true })
         .then(stream => {
           this.stream = stream
-          that.recorder.init(stream)
+          that.initWave(stream, audioContext) // 初始化音频波形图
+          that.recorder.init(stream) // 初始化音频监听
         })
-        .catch(err => console.log('Uh oh... unable to get stream...', err))
+        .catch(err => console.log('呃, ...你的设备貌似没有...麦克风?', err))
     },
 
-    // 关闭设备
+    // 关闭设备 TODO
     destroyMedia() {
       navigator.mediaDevices
         .getUserMedia({ audio: true })
         .then(stream => {
-          debugger
+          // https://developers.google.com/web/updates/2015/07/mediastream-deprecations?hl=en#stop-ended-and-active
           let track = stream.getTracks()[0] // if only one media track
           track.stop()
         })
-        .catch(err => console.log('Uh oh... unable to get stream...', err))
+        .catch(err => console.log('呃, ...你的设备貌似没有...麦克风?', err))
 
       this.recorder = null
     },
@@ -93,6 +104,9 @@ export default {
         this.recorder.stop().then(({blob, buffer}) => {
           that.isRecording = false
           that.blob = blob
+          let fd = new FormData()
+          fd.append('audioData', blob)
+          // 发送fd到后端 语音识别
           // buffer is an AudioBuffer
         })
       } catch (e) {
@@ -103,6 +117,45 @@ export default {
     // 录音下载
     downloadMedia() {
       Recorder.download(this.blob, 'my-audio-file') // downloads a .wav file
+    },
+
+    // 监听音频流
+    initWave(stream, audioContext) {
+      let visualizer = this.$refs.visualizer
+      let mask = this.$refs.mask
+      this.drawWave(stream, audioContext, visualizer, mask)
+    },
+
+    drawWave(stream, audioContext, visualizer, mask) {
+      let audioContent = audioContext // new (window.AudioContext || window.webkitAudioConetxt)()
+      let audioStream = audioContent.createMediaStreamSource(stream)
+      let analyser = audioContent.createAnalyser()
+      audioStream.connect(analyser)
+      analyser.fftSize = 1024
+
+      let frequencyArray = new Uint8Array(analyser.frequencyBinCount)
+      visualizer.setAttribute('viewBox', '0 0 255 255')
+
+      // Through the frequencyArray has a length longer than 255, there seems to be no
+      // significant data after this point. Not worth visualizing.
+      let path
+      for (let i = 0; i < 255; i++) {
+        path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+        path.setAttribute('stroke-dasharray', '4,1')
+        mask.appendChild(path)
+      }
+      let paths = document.getElementsByTagName('path')
+
+      let doDraw = () => {
+        requestAnimationFrame(doDraw)
+        analyser.getByteFrequencyData(frequencyArray)
+        let adjustedLength
+        for (let i = 0; i < 255; i++) {
+          adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5)
+          paths[i].setAttribute('d', 'M ' + (i) + ',255 l 0,-' + adjustedLength)
+        }
+      }
+      doDraw()
     }
   }
 }
@@ -111,7 +164,31 @@ export default {
 <style lang="less">
 @import '../../common/css/theme.less';
 
-.test {
-  color: @base-color;
+.recorder-js-demo {
+  // color: @base-color;
+
+  .wave-stream-demo {
+    padding: 0;
+    margin: 0;
+    width: 400px;
+    height: 300px;
+    background-color:#fff;
+    font-size: 0;
+    position: relative;
+  }
+  svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    position:absolute;
+  }
+
+  path {
+    stroke-linecap: square;
+    stroke: white;
+    stroke-width: 0.5px;
+  }
 }
 </style>
